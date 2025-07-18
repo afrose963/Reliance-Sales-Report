e3d6709a3ee3ad531b83bcfb32127dde3a910f81
afrose963 <afrose963@gmail.com>
2025-07-19 04:37:07 +0530

    Add files via upload

---
+++ Reliance-Sales-Report/create_project_structure.bat
@@ -0,0 +1,11 @@
+@echo off
+mkdir "Reliance-Sales-Report"
+cd "Reliance-Sales-Report"
+mkdir Dataset
+mkdir Report
+mkdir Screenshots
+mkdir Documentation
+echo # Reliance Sales Report > README.md
+cd Documentation
+echo # Data Dictionary > data_dictionary.md
+echo # Business Insights > business_insights.md
